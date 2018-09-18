82ac50937e2c94d6fc2e13258a37be24bdc86500
Gretarkri <gretarkri@gmail.com>
2018-09-18 10:52:42 +0000

    Added algorithm as comment

--- tiletraveler.py
+++ tiletraveler.py
@@ -1,1 +1,5 @@
-#bleblebel
+# skref 1: segja notenda frá hreyfingarmöguleikum.
+# skref 2: taka inn 'n', 's', 'e' eða 'w' og færa leikmann.
+# skref 3: ef leikmaður slær inn ógilda átt skal prenta út viðeigandi skilaboð og leyfa leikmanni að slá aftur inn átt.
+# skref 4: gera while lykkju sem fer í gegnum if setningar þar sem while greinir hvaða reit leikmaður er á.
+# skref 5: þegar leikmaður hefur komist á endareit, 3.1 þá prentum við út sigursetningu og lokuð prógramminu.
